b6f5ed3025994e57e625acc0c35928749286349c
Nelson Ortiz C <nelso570@gmail.com>
2021-05-25 22:18:08 -0500

    Two fer

---
+++ two-fer/two-fer.js
@@ -0,0 +1,10 @@
+//
+// This is only a SKELETON file for the 'Two fer' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const twoFer = (nombre = 'you') => {
+  return `One for ${nombre}, one for me.`;
+};
+
+
